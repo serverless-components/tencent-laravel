1f04e4129b44782f71ba65e049c1e647d7d472bd
Avril Li <linxforever@vip.qq.com>
2020-07-01 11:40:53 +0800

    chore: update test case

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -3,7 +3,7 @@ const path = require('path')
 const axios = require('axios')
 
 // set enough timeout for deployment to finish
-jest.setTimeout(30000)
+jest.setTimeout(300000)
 
 // the yaml file we're testing against
 const instanceYaml = {
@@ -13,8 +13,9 @@ const instanceYaml = {
   name: `laravel-integration-tests-${generateId()}`,
   stage: 'dev',
   inputs: {
-    runtime: 'Php7'
-    // region: 'ap-guangzhou'
+    runtime: 'Php7',
+    region: 'ap-guangzhou',
+    apigatewayConf: { environment: 'test' }
   }
 }
 
@@ -26,36 +27,14 @@ const credentials = {
 // get serverless construct sdk
 const sdk = getServerlessSdk(instanceYaml.org)
 
-// clean up the instance after tests
-afterAll(async () => {
-  await sdk.remove(instanceYaml, credentials)
-})
-
 it('should successfully deploy laravel app', async () => {
   const instance = await sdk.deploy(instanceYaml, { tencent: {} })
   expect(instance).toBeDefined()
   expect(instance.instanceName).toEqual(instanceYaml.name)
+  expect(instance.outputs).toBeDefined()
   // get src from template by default
   expect(instance.outputs.templateUrl).toBeDefined()
-})
-
-it('should successfully update basic configuration', async () => {
-  instanceYaml.inputs.region = 'ap-shanghai'
-  instanceYaml.inputs.functionName = 'laravelDemoTest'
-
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
-  expect(instance.outputs.scf).toBeDefined()
-  expect(instance.outputs.scf.functionName).toEqual(instanceYaml.inputs.functionName)
-})
-
-it('should successfully update apigatewayConf', async () => {
-  instanceYaml.inputs.apigatewayConf = { environment: 'test' }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.apigw).toBeDefined()
   expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
 })
@@ -71,14 +50,6 @@ it('should successfully update source code', async () => {
   expect(response.data.includes('Laravel')).toBeTruthy()
 })
 
-it('should successfully disable auto create api gateway', async () => {
-  instanceYaml.inputs.apigatewayConf = { isDisabled: true }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
-  expect(instance.outputs.apigw).not.toBeDefined()
-})
-
 it('should successfully remove laravel app', async () => {
   await sdk.remove(instanceYaml, credentials)
   result = await sdk.getInstance(
@@ -88,6 +59,5 @@ it('should successfully remove laravel app', async () => {
     instanceYaml.name
   )
 
-  // remove action won't delete the service cause the apigw have the api binded
   expect(result.instance.instanceStatus).toEqual('inactive')
 })
